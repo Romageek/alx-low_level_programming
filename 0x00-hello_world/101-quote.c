ec1578f1830c2ad405684995ca4388c96db5fe5a
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-06-30 18:37:41 -0700

    Updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long l = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall"
+	__asm__("syscall"
 		: "=a" (ret)
 		: "a" (syscall),
 		"D" (fd),
