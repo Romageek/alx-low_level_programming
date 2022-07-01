e6618b024111849ba1b2fda758b4fb66e35cc7a1
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-06-30 18:34:40 -0700

    Updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,23 @@
 
 /**
 * main - Printing
-*
-* Return 0
+* Description: Prints without using printf or puts
+* Return: 1
 */
 
 
 int main(void)
 {
-
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+	__asm__ ("syscall"
+		: "=a" (ret)
+		: "a" (syscall),
+		"D" (fd),
+		"S" (s),
+		"d" (l));
+	return (1);
+}
