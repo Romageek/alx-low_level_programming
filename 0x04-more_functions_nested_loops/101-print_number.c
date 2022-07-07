587cc2e1a9bd5d8d03dc115f8027f8a5dda2ab61
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-07-07 08:57:16 -0700

    Fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,12 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -n;
 	}
 	if (num / 10 != 0)
-	{
 		print_number(num / 10);
-		_putchar'0' + ((num % 10));
-	}
+	_putchar('0' + (num % 10));
 }
