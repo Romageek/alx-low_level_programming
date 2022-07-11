10f09cdc7eba08d6bc633621e66e4358516f02da
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-07-11 16:45:21 -0700

    Fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@
 
 void puts_half(char *str)
 {
-	int l = 1, half;
+	int l = 0, half;
 
 	while (*(str + l) != '\0')
 		l++;
 	half = l / 2;
 	if (l % 2)
-		half = (l - 1) / 2;
-	while (half <= l)
+		half = (l + 1) / 2;
+	while (half < l)
 	{
 		_putchar(str[half]);
 		half++;
