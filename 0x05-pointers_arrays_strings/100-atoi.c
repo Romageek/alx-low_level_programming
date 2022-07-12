13bc8dd598a018fc386dee04dc6d5205cdd0f6d4
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-07-11 18:12:34 -0700

    Fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 
 			c = *(s + i) - '0';
 			number = (number * 10) + c;
-			if ((*(s + i + 1) - '0') > 9 && (*(s + i + 1) - '0') <128)
+			if ((*(s + i + 1)  == ' ')
 				break;
 		}
 		i++;
