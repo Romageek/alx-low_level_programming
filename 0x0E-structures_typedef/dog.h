01a4a13c9193816da9337928e8a3ea253e68becc
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-08-01 17:46:55 -0700

    Fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Dog
  * @name: Dog's name
@@ -23,3 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
