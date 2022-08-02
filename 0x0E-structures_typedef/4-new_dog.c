bea1061ddcbab12f56e08119d368c31de7301255
Gnouyadou R. MAZNA <mazgnoucontact@gmail.com>
2022-08-01 18:07:51 -0700

    Fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int len_n = 0, len_o = 0, i;
+	dog_t *dog;
 
 	while (*(name + len_n))
 		len_n++;
@@ -20,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (*(owner + len_o))
 		len_o++;
 
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
